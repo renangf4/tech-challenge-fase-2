de322041fc99cba1edf79ec3b8e6adcf34a1f685
Renan Morais <renanmorais@live.com>
2025-07-24 21:27:37 -0300

    Feito pente fino do projeto

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -4,21 +4,17 @@ import { autentication } from "../helpers/auth.js";
 
 const postRoutes = express.Router();
 
-// Busca de posts
 postRoutes.get("/v1/posts/search", autentication, searchPosts);
 
-//Rotas simples
 postRoutes.route("/v1/posts")
   .get(autentication, getAllPosts)
   .post(autentication, postPost)
 
-//Rotas com parâmetros de ID
 postRoutes.route("/v1/posts/:id")
   .get(autentication, getPostById)
   .put(autentication, updatePost)
   .delete(autentication, deletePost)
 
-//Rotas de excessão
 postRoutes.all("/v1", erroRoute)
 
 export default postRoutes;
